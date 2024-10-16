c766dfda54acb4c440e2b68810e28c7b29fc9e6f
miletocarmelo <carmelo.mileto@gmail.com>
2024-10-16 21:09:43 +0200

    u

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ gui = Gui(pages=pages, css_file="styles.css")
 gui.add_page("root", layout)
 
 # Define a WSGI-compatible application
-app = gui.run.run
+app = gui.run
 
 if __name__ == "__main__":
     # gui.run(debug=True, dark_mode=True, use_reloader=True, title="Strategies creator")
